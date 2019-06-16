9d4068584c14b08c1278df9adc23c1dcf4db4f74
gccris <gabrielccristiano@gmail.com>
2019-06-16 19:42:31 -0300

    Casos de teste

---
+++ Projeto 2/src/Tests/UnitTest.java
@@ -0,0 +1,142 @@
+package Tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import model.Account;
+
+public class UnitTest {
+
+	@Test
+	public void testGender() {
+		boolean result;
+		result = Account.checkGender("male");
+		assertTrue(result);
+		result = Account.checkGender("Female");
+		assertTrue(result);
+		result = Account.checkGender("Male");
+		assertTrue(result);
+		result = Account.checkGender("female");
+		assertTrue(result);
+		result = Account.checkGender("homem");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testAge() {
+		boolean result;
+		result = Account.checkAge(10);
+		assertFalse(result);
+		result = Account.checkAge(18);
+		assertTrue(result);
+		result = Account.checkAge(30);
+		assertTrue(result);
+		result = Account.checkAge(110);
+		assertTrue(result);
+		result = Account.checkAge(120);
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testStringInt() {
+		boolean result;
+		result = Account.checkStringInt("10");
+		assertTrue(result);
+		result = Account.checkStringInt("10EB");
+		assertFalse(result);
+	
+	}
+	
+	@Test
+	public void testStringLong() {
+		boolean result;
+		result = Account.checkStringLong("10");
+		assertTrue(result);
+		result = Account.checkStringLong("10EB24D");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testNumberAccount() {
+		boolean result;
+		result = Account.checkAccountNumber("10");
+		assertTrue(result);
+		result = Account.checkAccountNumber("902183092183029183");
+		assertFalse(result);
+		result = Account.checkAccountNumber("");
+		assertFalse(result);
+		result = Account.checkAccountNumber("000000");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testName() {
+		boolean result;
+		result = Account.checkName("Abidal Valys");
+		assertTrue(result);
+		result = Account.checkName("");
+		assertFalse(result);
+		result = Account.checkName("Teste oasdieisdjsd sidjsidsidj imjdisjdis asiudhasiudhsaudh oiasjdiasdojais jdsd sijdsidjisd isndjisa");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testAadhar() {
+		boolean result;
+		result = Account.checkAadhar("214121355896");
+		assertTrue(result);
+		result = Account.checkAadhar("");
+		assertFalse(result);
+		result = Account.checkAadhar("0000000000000000");
+		assertFalse(result);
+		result = Account.checkAadhar("21452533");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testPanNo() {
+		boolean result;
+		result = Account.checkPanNo("123ABC123D");
+		assertTrue(result);
+		result = Account.checkPanNo("123ABC123");
+		assertFalse(result);
+		result = Account.checkPanNo("123ABC123DDD");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testAmount() {
+		boolean result;
+		result = Account.checkAmount("0");
+		assertTrue(result);
+		result = Account.checkAmount("10");
+		assertTrue(result);
+		result = Account.checkAmount("-20");
+		assertFalse(result);
+	}
+	public void testEmail() {
+		boolean result;
+		result = Account.checkEmail("test@email.com");
+		assertTrue(result);
+		result = Account.checkAmount("testgmail.com");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testAddress() {
+		boolean result;
+		result = Account.checkAddress("Rua dois, treis");
+		assertTrue(result);
+		result = Account.checkAddress("");
+		assertFalse(result);
+		result = Account.checkAddress("Teste oasdieisdjsd sidjsidsidj imjdisjdis asiudhasiudhsaudh oiasjdiasdojais jdsd sijdsidjisd isndjisa");
+		assertFalse(result);
+	}
+	
+	
+	
+
+}
--- Projeto 2/src/model/Account.java
+++ Projeto 2/src/model/Account.java
@@ -12,8 +12,9 @@ public class Account {
 	private int age;
 	private String gender;
 	private String email;
-	
-	public Account(long number, String name, long aadhar,String panNo, long amount, String address, int age,String gender, String email) {
+
+	public Account(long number, String name, long aadhar, String panNo, long amount, String address, int age,
+			String gender, String email) {
 		super();
 		this.number = number;
 		this.name = name;
@@ -25,69 +26,156 @@ public class Account {
 		this.gender = gender;
 		this.panNo = panNo;
 	}
-	
-	public Account() {}
-	
+
+	public Account() {
+	}
+
 	public long getNumber() {
 		return number;
 	}
+
 	public void setNumber(long number) {
 		this.number = number;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public long getAadhar() {
 		return aadhar;
 	}
+
 	public void setAadhar(long aadhar) {
 		this.aadhar = aadhar;
 	}
+
 	public long getAmount() {
 		return amount;
 	}
+
 	public void setAmount(long amount) {
 		this.amount = amount;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPanNo() {
 		return panNo;
 	}
+
 	public void setPanNo(String panNo) {
 		this.panNo = panNo;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
-	public boolean checkGender(String gender) {
+
+	public static boolean checkGender(String gender) {
 		if (gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female"))
 			return true;
 		return false;
 	}
+
+	public static boolean checkAge(int age) {
+		if (age < 18 || age > 110)
+			return false;
+		return true;
+
+	}
+
+	public static boolean checkStringInt(String value) {
+		try {
+			Integer.parseInt(value);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
 	
+	public static boolean checkStringLong(String value) {
+		try {
+			Long.valueOf(value);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
 	
+	public static boolean checkAccountNumber(String number) {
+		if (number.length() <=8 && number.length() > 0 && Long.parseLong(number) != 0)
+			return true;
+		return false;
+	
+	}
+	
+	public static boolean checkName(String name) {
+		if(name.length() > 0 && name.length() <= 100)
+			return true;
+		return false;
+	
+	}
+	
+	public static boolean checkAadhar(String aadhar) {
+		if(aadhar.length() == 12  && Long.parseLong(aadhar) != 0)
+			return true;
+		return false;
+	}
+	
+	public static boolean checkPanNo(String number) {
+		if(number.length() == 10)
+			return true;
+		return false;
+	}
+	
+	public static boolean checkAmount(String number) {
+		if(Long.parseLong(number) < 0)
+			return false;
+		return true;
+	}
+	
+	public static boolean checkEmail(String email) {
+		if(email.contains("@"))
+				return true;
+		return false;
+	}
+	
+	public static boolean checkAddress(String address) {
+		if(address.length() <= 100 && address.length() > 0)
+			return true;
+		return false;
+	}
+	
+
 }
